d28abc6b0da6af779a94094832c7d89bc99828a1
Rafik Bedreddine <bedreddinerafik@gmail.com>
2026-02-06 10:37:48 -0500

    script ajoute

---
+++ documents/GDH5.sql
@@ -0,0 +1,177 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gestion_horaires
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `affectation_cours`
+--
+
+DROP TABLE IF EXISTS `affectation_cours`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `affectation_cours` (
+  `id_affectation_cours` int NOT NULL AUTO_INCREMENT,
+  `id_cours` int NOT NULL,
+  `id_professeur` int NOT NULL,
+  `id_salle` int NOT NULL,
+  `id_plage_horaires` int NOT NULL,
+  PRIMARY KEY (`id_affectation_cours`),
+  KEY `fk_affcours_cours` (`id_cours`),
+  KEY `fk_affcours_professeur` (`id_professeur`),
+  KEY `fk_affcours_salle` (`id_salle`),
+  KEY `fk_affcours_plage` (`id_plage_horaires`),
+  CONSTRAINT `fk_affcours_cours` FOREIGN KEY (`id_cours`) REFERENCES `cours` (`id_cours`),
+  CONSTRAINT `fk_affcours_plage` FOREIGN KEY (`id_plage_horaires`) REFERENCES `plages_horaires` (`id_plage_horaires`),
+  CONSTRAINT `fk_affcours_professeur` FOREIGN KEY (`id_professeur`) REFERENCES `professeurs` (`id_professeur`),
+  CONSTRAINT `fk_affcours_salle` FOREIGN KEY (`id_salle`) REFERENCES `salles` (`id_salle`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `affectation_groupes`
+--
+
+DROP TABLE IF EXISTS `affectation_groupes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `affectation_groupes` (
+  `id_affectation_groupes` int NOT NULL AUTO_INCREMENT,
+  `id_groupes_etudiants` int NOT NULL,
+  `id_affectation_cours` int NOT NULL,
+  PRIMARY KEY (`id_affectation_groupes`),
+  KEY `fk_affgroup_groupe` (`id_groupes_etudiants`),
+  KEY `fk_affgroup_affcours` (`id_affectation_cours`),
+  CONSTRAINT `fk_affgroup_affcours` FOREIGN KEY (`id_affectation_cours`) REFERENCES `affectation_cours` (`id_affectation_cours`),
+  CONSTRAINT `fk_affgroup_groupe` FOREIGN KEY (`id_groupes_etudiants`) REFERENCES `groupes_etudiants` (`id_groupes_etudiants`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cours`
+--
+
+DROP TABLE IF EXISTS `cours`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cours` (
+  `id_cours` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nom` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `duree` int NOT NULL,
+  `programme` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `etape_etude` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type_salle` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `archive` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_cours`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `groupes_etudiants`
+--
+
+DROP TABLE IF EXISTS `groupes_etudiants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `groupes_etudiants` (
+  `id_groupes_etudiants` int NOT NULL AUTO_INCREMENT,
+  `nom_groupe` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_groupes_etudiants`),
+  UNIQUE KEY `nom_groupe` (`nom_groupe`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `plages_horaires`
+--
+
+DROP TABLE IF EXISTS `plages_horaires`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `plages_horaires` (
+  `id_plage_horaires` int NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `heure_debut` time NOT NULL,
+  `heure_fin` time NOT NULL,
+  PRIMARY KEY (`id_plage_horaires`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `professeurs`
+--
+
+DROP TABLE IF EXISTS `professeurs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professeurs` (
+  `id_professeur` int NOT NULL AUTO_INCREMENT,
+  `matricule` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `specialite` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id_professeur`),
+  UNIQUE KEY `matricule` (`matricule`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `salles`
+--
+
+DROP TABLE IF EXISTS `salles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salles` (
+  `id_salle` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `capacite` int NOT NULL,
+  PRIMARY KEY (`id_salle`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utilisateurs` (
+  `id_utilisateur` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `motdepasse` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_utilisateur`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-06 10:27:54
